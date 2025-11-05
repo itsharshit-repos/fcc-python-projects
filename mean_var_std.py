4698be8da6eb94a073b0cc2316e6e1576f5c95ae
itsharshit-repos <harshit.me2006@gmail.com>
2025-11-05 20:00:36 +0530

    Add files via upload

---
+++ mean_var_std.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def calculate(num):
+    
+    if len(num) != 9:
+        raise ValueError("List must contain nine numbers..")
+    
+    matrix = np.array(num).reshape(3,3)
+
+    mean_var_std = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().item()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().item()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().item()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().item()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().item()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().item()]
+    }
+    return mean_var_std
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
